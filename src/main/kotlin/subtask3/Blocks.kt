cabb8bd965898b00bfeefa66b4bbda702f23a381
= <=>
2021-06-15 00:18:16 +0300

    init: add task Blocks

--- src/main/kotlin/subtask3/Blocks.kt
+++ src/main/kotlin/subtask3/Blocks.kt
@@ -1,11 +1,59 @@
 package subtask3
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
+    private var intElement = 0
+    private var stringElement: String = ""
+    private val myPattern = DateTimeFormatter.ofPattern("dd.MM.yyyy")
+    private var date = LocalDate.parse("0001-01-01")
 
-    // TODO: Complete the following function
     fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+        if (blockB == Int::class) {
+            for (element in blockA) {
+                if (element is Int) {
+                    intElement += element
+                }
+            }
+        }
+
+        if (blockB == String::class) {
+            for (element in blockA) {
+                if (element is String) {
+                    stringElement += element
+                }
+            }
+        }
+
+        if (blockB == LocalDate::class) {
+            blockA.forEach { any:Any? ->
+                when (any) {
+                    is LocalDate -> if (any.isAfter(date)) date = any
+                }
+            }
+        }
+
+        return when (blockB) {
+            Int::class -> intElement
+            String::class -> stringElement
+            LocalDate::class -> date.format(myPattern)
+            else -> "incur losses"
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
