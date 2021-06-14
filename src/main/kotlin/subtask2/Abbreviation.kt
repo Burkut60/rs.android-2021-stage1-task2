fb07165891d4702f750545aa195b47eb325568b8
= <=>
2021-06-14 22:00:24 +0300

    init: add task Abbreviation

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -1,9 +1,32 @@
 package subtask2
 
+import java.lang.StringBuilder
+
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val strUpper = a.toUpperCase()
+        val strToArrayA = strUpper.toCharArray()
+        val strToArrayB = b.toCharArray()
+
+        val listResult = arrayListOf<String>()
+
+        for (i in strToArrayA) {
+            for (j in strToArrayB) {
+                if (i == j) {
+                    listResult.add(i.toString())
+                }
+            }
+        }
+        val builder = StringBuilder()
+        for (a in listResult) {
+            builder.append(a)
+        }
+        return if (builder.toString() == b) {
+            "YES"
+        } else {
+            "NO"
+        }
     }
 }
